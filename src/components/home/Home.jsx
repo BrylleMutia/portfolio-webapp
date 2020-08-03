52198c6fda7ab86ac0509e7af29a8e53f38b8b99
Brylle Mutia <bryllemutia3@gmail.com>
2020-08-03 18:03:20 +0800

    added animations for homepage

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -9,6 +9,9 @@ function Home() {
             <div className={classes.secondline}><h3>BRYLLE MUTIA</h3></div>
             <div className={classes.thirdline}><hr/><h6>FULLSTACK WEB DEVELOPER</h6></div>
             <div className={classes.fourthline}><button>GET TO KNOW ME</button></div>
+
+            <div className={classes.slant_left}></div>
+            <div className={classes.slant_right}></div>
         </main>
     )
 }
