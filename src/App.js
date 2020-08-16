6e268b84ecc1e46622a1a3d1d3d8a14f6678b16b
Brylle Mutia <bryllemutia3@gmail.com>
2020-08-16 18:10:01 +0800

    fixed responsiveness on chart display mode on skills section

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
+import classes from "./App.module.scss";
+
 import Navbar from "./components/navbar/Navbar";
 import Home from './components/home/Home';
 import Bio from "./components/bio/Bio";
 import Skills from "./components/skills/Skills";
 import Contact from "./components/contact/Contact";
 import Portfolio from "./components/portfolio/Portfolio";
-import classes from "./App.module.scss";
 
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
-  const [location, setLocation] = useState("Home");
-
-  const handleSetLocation = (link) => setLocation(link);
-
   return (
     <div className={classes.app}>
       <Router>
@@ -21,7 +18,6 @@ function App() {
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/bio" exact component={Bio} />
-          {/* <Route path="/skills" exact component={Chart} /> */}
           <Route path="/skills" exact component={Skills} />
           <Route path="/portfolio" exact component={Portfolio} />
           <Route path="/contact" exact component={Contact} />
